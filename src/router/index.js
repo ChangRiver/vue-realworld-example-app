4409b0710178cfe6b82a49f74006340f2ae904f2
changriver <changriver@suboat.com>
2017-04-24 15:07:52 +0800

    add route defend

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,11 @@ import Settings from '../views/Settings/Settings';
 import Article from '../views/Article/Article';
 import Profile from '../views/Profile/Profile';
 import ProfileFavorites from '../views/Profile/ProfileFavorites';
-import ProfileArticles from '../views/Profile/ProfileArticles';
 import Editor from '../views/Editor/Editor';
 
 Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  routes: [
+const routes = [
     {
       path: '/',
       name: 'home',
@@ -33,42 +30,45 @@ export default new Router({
     {
       path: '/settings',
       name: 'settings',
+      meta: {
+        requireAuth: true 
+      },
       component: Settings
     },
     {
       path: '/article/:id',
       name: 'article',
+      meta: {
+        requireAuth: true 
+      },
       component: Article
     },
-    // {
-    //   path: '/profile',
-    //   name: 'Profile',
-    //   component: Profile,
-    //   children: [
-    //     {
-    //       path: ':username',
-    //       component: ProfileArticles
-    //     },
-    //     {
-    //       path: ':username/favorites',
-    //       component: ProfileFavorites
-    //     }
-    //   ]
-    // },
     {
       path: '/@:username',
+      meta: {
+        requireAuth: true 
+      },
       component: Profile
     },
     {
       path: '/@:username/favorites',
+      meta: {
+        requireAuth: true 
+      },
       component: ProfileFavorites
     },
     {
       path: '/editor',
+      meta: {
+        requireAuth: true 
+      },
       component: Editor
     },
     {
       path: '/editor/:slug',
+      meta: {
+        requireAuth: true 
+      },
       component: Editor
     },
     {
@@ -79,5 +79,27 @@ export default new Router({
       path: '/@:username/article/:id',
       redirect: '/article/:id'
     }
-  ]
+  ];
+
+const router = new Router({
+  mode: 'history',
+  routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(r => r.meta.requireAuth)) {
+    if(localStorage.getItem('jwt')) {
+      next();
+    } else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
